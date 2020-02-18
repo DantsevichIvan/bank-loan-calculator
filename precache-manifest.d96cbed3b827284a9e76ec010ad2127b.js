5842f60bbd60617abb28c9a3a5819c90e98f9a99
DantsevichIvan <iw.dantsevich@gmail.com>
2020-02-18 14:19:09 +0300

    Updates

---
+++ precache-manifest.d96cbed3b827284a9e76ec010ad2127b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef94d1dc539045cf7260dcd0e95f1ef8",
+    "url": "/bank-loan-calculator/index.html"
+  },
+  {
+    "revision": "727f61d7dec412e52796",
+    "url": "/bank-loan-calculator/static/css/main.33d56128.chunk.css"
+  },
+  {
+    "revision": "65e7d1f27306979de4f2",
+    "url": "/bank-loan-calculator/static/js/2.65912663.chunk.js"
+  },
+  {
+    "revision": "727f61d7dec412e52796",
+    "url": "/bank-loan-calculator/static/js/main.195c810f.chunk.js"
+  },
+  {
+    "revision": "051cd1dcf3d8f0eaadbc",
+    "url": "/bank-loan-calculator/static/js/runtime-main.b5eecc13.js"
+  }
+]);
